741426bf17efb2274ddb0ce78c5173492ae1baca
Caio Toledo <caiobbgtoledo@gmail.com>
2026-01-26 14:53:44 -0300

    usecases and repo interface

--- src/server.py
+++ src/server.py
@@ -1,6 +1,8 @@
 from mcp.server.fastmcp import FastMCP
 from src.templates.entity import ENTITY_EXAMPLE, ENTITY_TEST_EXAMPLE
 from src.tools.entity import create_entity_tool, create_entity_test_tool
+from src.tools.repository_interface import create_repository_interface_tool
+from src.tools.usecase import create_usecase_tool, create_usecase_test_tool
 
 mcp = FastMCP("the-crud-maker")
 
@@ -21,6 +23,21 @@ def create_entity(entity_name: str, fields: dict[str, str]) -> str:
 async def create_entity_test(entity_name: str, fields: dict[str, str]) -> str:
     """Generates an entity test file content following the entity test example."""
     return await create_entity_test_tool(entity_name, fields)
+
+@mcp.tool(name="create_repository_interface")
+async def create_repository_interface(repository_name: str, methods: dict[str, str]) -> str:
+    """Generates an repository interface file content following the repository interface example."""
+    return await create_repository_interface_tool(repository_name, methods)
+
+@mcp.tool(name="create_usecase")
+async def create_usecase(module_name: str, usecase_name: str, business_rules: str) -> str:
+    """Generates an usecase file content following the usecase example."""
+    return await create_usecase_tool(module_name, usecase_name, business_rules)
+
+@mcp.tool(name="create_usecase_test")
+async def create_usecase_test(module_name: str, usecase_name: str) -> str:
+    """Generates an usecase test file content following the usecase test example."""
+    return await create_usecase_test_tool(module_name, usecase_name)
 
 def main():
     mcp.run()
---
+++ src/tools/usecase.py
@@ -0,0 +1,15 @@
+from src.templates.usecase import USECASE_EXAMPLE, USECASE_TEST_EXAMPLE
+
+def create_usecase_tool(module_name: str, usecase_name: str, business_rules: str) -> str:
+    """Generates an usecase file content following the usecase example."""
+    return f"""Create a usecase file in the folder /src/modules/{module_name}/app/{usecase_name} that have this business rules: {business_rules}.
+    Follow this example of usecase: {USECASE_EXAMPLE}
+    If the use case requires access to or modification of a repository, create it using the `create_repository_interface` tool or use an existing one.
+    After create the usecase, call the `create_usecase_test` tool
+    """
+
+async def create_usecase_test_tool(module_name: str, usecase_name: str) -> str:
+    """Generates an usecase test file content following the usecase test example."""
+    return f"""Create a usecase test file in the folder /tests/modules/{module_name}/app/{usecase_name} to covarage this file: /src/modules/{module_name}/app/{usecase_name}.
+    Follow this example of usecase test: {USECASE_TEST_EXAMPLE}
+    """
